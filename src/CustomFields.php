fdd1286880ce493e4e15f29767217f44a5b5f656
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-07 10:59:59 +0100

    Extract association type generation

--- src/CustomFields.php
+++ src/CustomFields.php
@@ -34,19 +34,19 @@ final class CustomFields implements Bootable
          */
         $postTypes = apply_filters('helick_related_posts_supported_post_types', ['post']);
 
+        $associationTypes = array_map(function (string $postType) {
+            return [
+                'type'      => 'post',
+                'post_type' => $postType,
+            ];
+        }, $postTypes);
+
         Container::make('post_meta', __('Related Posts', DOMAIN))
                  ->where('post_type', 'IN', (array)$postTypes)
                  ->add_fields([
                      Field::make('association', 'helick_related_posts', __('Manually selected related posts', DOMAIN))
                           ->set_help_text(__('By default, 10 related posts are dynamically generated.', DOMAIN))
-                          ->set_types([
-                              array_map(function (string $postType) {
-                                  return [
-                                      'type'      => 'post',
-                                      'post_type' => $postType,
-                                  ];
-                              }, $postTypes)
-                          ]),
+                          ->set_types([$associationTypes]),
                  ]);
     }
 }
