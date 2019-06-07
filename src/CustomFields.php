3f904a7c2f4d69551127c1055eba8101bda5afba
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-07 11:20:01 +0100

    Associated post types (#2)
    * Extract association type generation
    
    * Explicitly name supported post types
    
    * Control associated post types
    
    * Update hook docblock

--- src/CustomFields.php
+++ src/CustomFields.php
@@ -28,25 +28,32 @@ final class CustomFields implements Bootable
     public function register(): void
     {
         /**
-         * Control the post types that should get the related posts support.
+         * Control the supported post types.
          *
-         * @param array $postTypes
+         * @param array $supportedPostTypes
          */
-        $postTypes = apply_filters('helick_related_posts_supported_post_types', ['post']);
+        $supportedPostTypes = apply_filters('helick_related_posts_supported_post_types', ['post']);
+
+        /**
+         * Control the associated post types.
+         *
+         * @param array $associatedPostTypes
+         */
+        $associatedPostTypes = apply_filters('helick_related_posts_associated_post_types', ['post']);
+
+        $associationTypes = array_map(function (string $postType) {
+            return [
+                'type'      => 'post',
+                'post_type' => $postType,
+            ];
+        }, $associatedPostTypes);
 
         Container::make('post_meta', __('Related Posts', DOMAIN))
-                 ->where('post_type', 'IN', (array)$postTypes)
+                 ->where('post_type', 'IN', (array)$supportedPostTypes)
                  ->add_fields([
                      Field::make('association', 'helick_related_posts', __('Manually selected related posts', DOMAIN))
                           ->set_help_text(__('By default, 10 related posts are dynamically generated.', DOMAIN))
-                          ->set_types([
-                              array_map(function (string $postType) {
-                                  return [
-                                      'type'      => 'post',
-                                      'post_type' => $postType,
-                                  ];
-                              }, $postTypes)
-                          ]),
+                          ->set_types([$associationTypes]),
                  ]);
     }
 }
